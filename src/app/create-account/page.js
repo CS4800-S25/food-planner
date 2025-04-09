17a8f210190701a11c3ddfdb110e4b40f3355d35
GuyWhoCode <34526187+GuyWhoCode@users.noreply.github.com>
2025-04-09 15:20:16 -0700

    feat: init template pages

---
+++ src/app/create-account/page.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function CreateAccount() {
+    return (
+        <>
+            <h1>Create Account Workflow Page</h1>
+            <Link href="/">Return to Home Page</Link>
+        </>
+    );
+}
---
+++ src/app/view-meals/page.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+function ViewMeals() {
+    return (
+        <>
+            <h1> View Meals Workflow Page</h1>
+            <p> This is the page where users can view their meals.</p>
+            
+            <Link href="/">Return to Home Page</Link>
+        </>
+    );
+}
+
+export default ViewMeals;
