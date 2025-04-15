ab7a323f0be6c3023aeb717b9b0c8711bf75c735
Olena Molla <133937893+olenamolla@users.noreply.github.com>
2025-04-14 18:20:48 -0700

    feat(ui): add animated tagline carousel and enlarge login card

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,14 +1,27 @@
 "use client";
 
-import { useState } from "react"; //to create input states
+import { useState, useEffect } from "react"; //to create input states
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Card, CardHeader,CardTitle,CardContent,CardFooter,} from "@/components/ui/card";
 
 function LoginPage() {
 
-    //const [email, setEmail] = useState("");
-    //const [password, setPassword] = useState("");
+    const tagLines = [
+      "Plan Meals Smarter",
+      "Your Weekly Meals, Made Easy",
+      "Organize. Shop. Eat Well."
+    ];
+
+    const [currentTaglineIndex, setCurrentTaglineIndex] = useState(0);
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setCurrentTaglineIndex((prev) => (prev +1) % tagLines.length);
+      }, 1500);
+      return () => clearInterval(interval);
+    }, []);
+
 
     const handleGoogleSignIn = () => {
         SignIn("google", { callbackUrl: "/"});
@@ -20,7 +33,7 @@ function LoginPage() {
     return (
         
         <main className="min-h-screen flex items-center justify-center bg-rose-50 p-6">
-        <Card className="w-full max-w-xl bg-white/90 border-2 border-rose-100 shadow-lg">
+        <Card className="w-full max-w-3xl bg-white/90 border-2 border-rose-100 shadow-lg p-20">
           <CardHeader>
             <CardTitle className="text-center text-3xl text-rose-600">
             Welcome to Food Planner
@@ -28,12 +41,22 @@ function LoginPage() {
           </CardHeader>
   
           <CardContent className="space-y-6">
-            <Button
-              onClick = { handleGoogleSignIn }
-              className="w-full bg-rose-500 hover:bg-rose-600 text-white">
+              {/* Tagline */} 
+            
+              <div className="text-center text-rose-500 text-base font-medium min-h-[24px] transition-opacity duration-500 ease-in-out">
+                {tagLines[currentTaglineIndex]}
+              </div>
+            
+              {/* Google sign in button */}
+              <Button
+                onClick = { handleGoogleSignIn }
+                className="w-full bg-rose-500 hover:bg-rose-600 text-white">
 
-                 Sign In with Google
+                Sign In with Google
+
             </Button>
+
+
           </CardContent>
             
         </Card>
