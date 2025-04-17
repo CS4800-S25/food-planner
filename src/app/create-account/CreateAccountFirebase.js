cfdcee7ffd78884c9c57f4c6ba5748fa00fde6cb
Olena Molla <133937893+olenamolla@users.noreply.github.com>
2025-04-17 13:05:45 -0700

    feat: connect Firebase with user email

--- src/app/create-account/CreateAccountFirebase.js
+++ src/app/create-account/CreateAccountFirebase.js
@@ -1,9 +1,21 @@
 import { collection, addDoc } from "firebase/firestore";
 import { Button } from "@/components/ui/button";
 import db from "@/lib/firebase";
+import { useSession } from "next-auth/react";  
+import IngredientPreferences from "./(steps)/IngredientPreferences";
 
 async function createAccountFirebase() {
     const documentData = {
+        email: session?.user?.email,  // save the signed-in user's email
+
+        // fill with real data later
+        preferences: {
+            ingredientPreferences: ["avocado", "broccoli"],
+            healthGoals: "muscle gain",
+            numberOfMeals: 3,
+            budget: 35
+        },
+
         name: "Bob",
         numberOfMeals: Math.floor(Math.random() * 10) + 1,
     };
@@ -13,7 +25,12 @@ async function createAccountFirebase() {
 }
 
 function CreateAccountFirebase() {
-    return <Button onClick={createAccountFirebase}>Create Account</Button>;
+
+    const {data: session} = useSession();
+
+    return <Button onClick={createAccountFirebase(session)}>
+                Create Account
+           </Button>;
 }
 
 export default CreateAccountFirebase;
