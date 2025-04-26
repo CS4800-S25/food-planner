ffe5efc883993bd9881d38cc24af6fc0cdd71b72
GuyWhoCode <34526187+GuyWhoCode@users.noreply.github.com>
2025-04-25 23:09:26 -0700

    chore: clear git workspace

---
+++ src/app/api/generate-recipes/route.js
@@ -0,0 +1,16 @@
+// export const revalidate = 0; // Disable revalidation for this route
+// export async function POST(request) {
+//     const res = await request.json();
+//     if (!res) {
+//         return new Response("No data provided", { status: 400 });
+//     }
+//     const {
+//         budget,
+//         healthDetails,
+//         healthGoal,
+//         ingredientPreferences,
+//         numberOfMeals,
+//     } = res;
+
+    
+// }
